137a6cfe335c4821b4c970d0c0d4cdf3fbf6adff
Simon Guilbault <simon.guilbault@calculquebec.ca>
2021-05-04 18:36:01 +0000

    Connecting to slurmdb and ldap instead of running commands over ssh

--- lustre_exporter_slurm.py
+++ lustre_exporter_slurm.py
@@ -1,51 +1,61 @@
+import MySQLdb
+import ldap
 import base64
-import paramiko
-import csv
 from aiohttp import web
 import aiohttp
 import asyncio
 import re
 import json
-from cachetools import cached, LRUCache, TTLCache
+from cachetools import cached, LRUCache
 import configparser
 import sys
 
 async def handle(request):
     server = request.match_info.get('server')
     async with aiohttp.ClientSession() as session:
-        async with session.get('http://{}:9169/metrics'.format(server)) as resp:
+        url = 'http://{}:9169/metrics'.format(server)
+        async with session.get(url) as resp:
             metrics = await resp.text()
             text = str("\n".join(improve_metrics(metrics)))
     return web.Response(text=text)
 
-app = web.Application()
-app.add_routes([web.get('/{server}', handle)])
 
-@cached(cache=TTLCache(maxsize=10000, ttl=60))
-def get_jobs_info():
+@cached(cache=LRUCache(maxsize=100000))
+def get_job_info(jobid):
+    # Return the username and account used by this jobid
+    cursor = db.cursor(MySQLdb.cursors.DictCursor)
+    # Since the job table depends on how the slurm cluster was named, we need
+    # to replace the name with format, cursor.execute cannot replace the
+    # table name, only "normal" SQL parameters
+    query_string = "select id_user,account from {} where id_job=%s".format(
+        job_table)
+    cursor.execute(query_string, (jobid,))
+    result = cursor.fetchone()
     try:
-        stdin, stdout, stderr = client.exec_command('/opt/software/slurm/bin/squeue -t r --format %A,%u,%a')
-        reader = csv.reader(stdout)
-        jobs = {}
-        for row in reader:
-            jobs[row[0]] = {'user': row[1], 'account': row[2]}
-        return jobs
-    except paramiko.ssh_exception.SSHException as err:
-        print(err)
-        sys.exit(1)
+        return {
+            'user': get_username(result['id_user']),
+            'account': result['account'],
+        }
+    except:
+        return None
+
 
 @cached(cache=LRUCache(maxsize=10000))
 def get_username(uid):
-    try:
-        stdin, stdout, stderr = client.exec_command('/usr/bin/id --name --user {}'.format(uid))
-        return stdout.read().strip().decode('UTF-8')
-    except paramiko.ssh_exception.SSHException as err:
-        print(err)
-        sys.exit(1)
+    # Return the username, from a numerical uid
+    if int(uid) == 0:
+        return 'root'
+
+    result = ldap_conn.search_s(
+        'ou=People,dc=computecanada,dc=local',
+        ldap.SCOPE_SUBTREE,
+        '(uidNumber={})'.format(uid),
+        ['uid'])
+    return result[0][1]['uid'][0].decode('utf-8')
+
 
 def improve_metrics(metrics):
     lines = []
-    jobs_info = get_jobs_info()
     for line in metrics.splitlines():
         # On each metric line, extract the name, labels and value
         m1 = re.match(r'(lustre_job.*){(.*)} (.*)', line)
@@ -69,8 +79,8 @@ def improve_metrics(metrics):
                 if labels_d['jobid'].isnumeric():
                     # slurm jobid
                     try:
-                        labels_d.update(jobs_info[labels_d['jobid']])
-                    except KeyError:
+                        labels_d.update(get_job_info(labels_d['jobid']))
+                    except TypeError:
                         pass
                 else:
                     # login node or mgmt node with procname.uid
@@ -83,7 +93,7 @@ def improve_metrics(metrics):
                 # Repackage the new labels
                 a = []
                 for key, value in labels_d.items():
-                    a.append('{}="{}"'.format(key,value))
+                    a.append('{}="{}"'.format(key, value))
                 b = ",".join(a)
                 lines.append(metric_name + "{" + b + "} " + metric_value)
             else:
@@ -94,16 +104,23 @@ def improve_metrics(metrics):
     return lines
 
 if __name__ == '__main__':
+    app = web.Application()
+    app.add_routes([web.get('/{server}', handle)])
+
     config = configparser.ConfigParser()
     if len(sys.argv) > 1:
         config.read(sys.argv[1])
     else:
         config.read('config.ini')
 
-    private_key = paramiko.RSAKey.from_private_key_file(config.get('ssh', 'private_key'))
-    client = paramiko.SSHClient()
-    client.load_system_host_keys()
-    client.connect(config.get('ssh', 'host'), username=config.get('ssh', 'user'), pkey=private_key)
+    db = MySQLdb.connect(
+        host=config.get('slurmdb', 'host'),
+        port=int(config.get('slurmdb', 'port')),
+        user=config.get('slurmdb', 'user'),
+        password=config.get('slurmdb', 'password'),
+        db=config.get('slurmdb', 'dbname'))
+    job_table = config.get('slurmdb', 'job_table')
 
+    ldap_conn = ldap.initialize(config.get('ldap', 'server'))
+    ldap_search_base = config.get('ldap', 'search_base')
     web.run_app(app, port=config.get('api', 'local_port'))
-    client.close()
